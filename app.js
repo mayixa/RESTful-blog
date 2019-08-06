bd1a5d96b6c2202225c295fe2509195626f9fbbd
Emma <emmaborghult@gmail.com>
2019-08-06 14:45:09 +0200

    Connect to DB + create blogSchema for mongoose

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ const express = require("express");
       bodyParser = require("body-parser");
       mongoose = require("mongoose");
 
+mongoose.connect('mongodb+srv://Mayixa:flingan95@mayixa-avcru.azure.mongodb.net/blog?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to mongoDB!')
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+app.listen(3000, () => {
+    console.log("Blog Server Started");
+});
